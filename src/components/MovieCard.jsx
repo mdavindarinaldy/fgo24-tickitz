8fd521f76e29bed52a49a4e014f7178f0777c871
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-08 18:31:46 +0700

    feat: add fallback image placeholder in movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import placeholder from '../assets/image-placeholder.png'
 
 function MovieCard({id, src, width, height, name, genre, textSize, buttonSize, date, details, ...props}) {
   const genres=[]
+  const [imageError,setImageError] = useState(false)
+
   function addGenre(item) {
     if (item === '28') genres.push('Action');
     if (item === '12') genres.push('Adventure');
@@ -24,30 +27,34 @@ function MovieCard({id, src, width, height, name, genre, textSize, buttonSize, d
     if (item === '10752') genres.push('War');       
     if (item === '37') genres.push('Western');
   }
-
-  let anchor=''
   genre.forEach((item) => addGenre(item))
-  if (!src.includes('null')) {
-    if (details===true) { anchor = `/buy-ticket/${id}` }
-    else if (details===false) { anchor = `/` }
-    return (
-        <Link to={anchor}>
-            <div className='flex flex-col justify-center items-center mb-10 gap-2' {...props}>
-              <img src={src} alt="poster-movie" className={`max-w-none rounded-3xl object-cover ${width} ${height} hover:border-10 hover:border-orange-500`}/>
-              <span className={`font-bold ${textSize} text-center`}>{name}</span>
-              <div className='flex flex-row justify-center gap-5 max-w-svw lg:max-w-[20svw]'>
-                  {date === '' ? (
-                      genres.map((item, index) => (
-                          <div key={`movie-card-${index}`} className={`bg-gray-300 rounded-2xl py-1 px-5 font-normal ${buttonSize} text-gray-600 w-[50%] lg:w-fit`}>{item}</div>
-                      ))
-                  ) : (
-                      <div className={`bg-orange-100 rounded-3xl text-orange-500 font-bold text-[10px] w-fit py-2 px-3`}>{date}</div>
-                  )}
-              </div>
-            </div>
-        </Link>
-      )
-  }
+  
+  let anchor=''
+  if(details===true) {anchor = `/buy-ticket/${id}`}
+  else if (details===false) { anchor = '/'}
+
+  return (
+    <Link to={anchor}>
+        <div className='flex flex-col justify-center items-center mb-10 gap-2' {...props}>
+          <img 
+            src={imageError ? placeholder : src} 
+            alt="poster-movie" 
+            className={`max-w-none rounded-3xl object-cover ${width} ${height} hover:border-10 hover:border-orange-500`}
+            onError={()=>setImageError(true)}
+          />
+          <span className={`font-bold ${textSize} text-center`}>{name}</span>
+          <div className='flex flex-row justify-center gap-5 max-w-svw lg:max-w-[20svw]'>
+              {date === '' ? (
+                  genres.map((item, index) => (
+                      <div key={`movie-card-${index}`} className={`bg-gray-300 rounded-2xl py-1 px-5 font-normal ${buttonSize} text-gray-600 w-[50%] lg:w-fit`}>{item}</div>
+                  ))
+              ) : (
+                  <div className={`bg-orange-100 rounded-3xl text-orange-500 font-bold text-[10px] w-fit py-2 px-3`}>{date}</div>
+              )}
+          </div>
+        </div>
+    </Link>
+  )
 }
 
 export default MovieCard
